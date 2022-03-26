2a6cf8e036c3bbe4f2c10cfa1c6ff56abcd29be7
r__r <rpl159@laposte.net>
2022-03-26 16:57:16 +0100

    networks from paper

--- src/networks.py
+++ src/networks.py
@@ -45,3 +45,44 @@ class PolicyNetwork(hk.Module):
         mu, pre_sigma = jnp.split(h, 2, axis=-1)
         sigma = jax.nn.softplus(pre_sigma)
         return hk.Reshape(action_shape)(.1 * mu), hk.Reshape(action_shape)(.1 * sigma)
+
+
+class ValueNetworkDDPG(hk.Module):
+    def __init__(self, name: Optional[str] = None) -> None:
+        super().__init__(name=name)
+
+    def __call__(self, s: chex.Array, a: chex.Array, is_training: bool) -> chex.Array:
+        h = s
+        h = hk.BatchNorm(create_scale=True, create_offset=True, decay_rate=0.99)(h, is_training)
+
+        h = hk.Linear(400)(h)
+        h = hk.BatchNorm(create_scale=True, create_offset=True, decay_rate=0.99)(h, is_training)
+        h = jax.nn.relu(h)
+
+        h = hk.Linear(300)(jnp.concatenate([h,a], axis=1))
+        h = jax.nn.relu(h)
+
+        return hk.Linear(1, hk.initializers.RandomUniform(-3e-3, 3e-3))(h)[..., 0]
+
+class PolicyNetworkDDPG(hk.Module):
+    def __init__(self, action_spec: specs.BoundedArray, name: Optional[str] = None) -> None:
+        super().__init__(name=name)
+        self._action_spec = action_spec
+
+    def __call__(self, x: chex.Array, is_training: bool) -> chex.Array:
+        action_shape = self._action_spec.shape
+        action_dims = np.prod(action_shape)
+        h = x
+        h = hk.BatchNorm(create_scale=True, create_offset=True, decay_rate=0.99)(h, is_training)
+
+        h = hk.Linear(400)(h)
+        h = hk.BatchNorm(create_scale=True, create_offset=True, decay_rate=0.99)(h, is_training)
+        h = jax.nn.relu(h)
+
+        h = hk.Linear(300)(h)
+        h = hk.BatchNorm(create_scale=True, create_offset=True, decay_rate=0.99)(h, is_training)
+        h = jax.nn.relu(h)
+
+        h = hk.Linear(action_dims, hk.initializers.RandomUniform(-3e-3, 3e-3))(h)
+        h = jnp.tanh(h)
+        return hk.Reshape(action_shape)(h)
